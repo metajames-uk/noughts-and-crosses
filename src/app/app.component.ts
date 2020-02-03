b7eef6c94eb997fbaa97c153579ce0834ecbf121
James Hollands <james.hollands@pushdr.com>
2020-02-03 19:37:59 +0000

    getting game working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  turnIsX = true;
   title = 'noughts-and-crosses';
+  squares = ['', '', '', '', '', '', '', '', ''];
+  results = {
+    x: [],
+    o: []
+  };
+
+  calculateWin(turn) {
+    console.log(turn);
+  }
+
+  buttonSelected(e) {
+    this.squares[e] = this.turnIsX ? 'X' : 'O';
+    this.calculateWin(e);
+    this.turnIsX = !this.turnIsX;
+  }
+  newGame() {
+    this.squares = ['', '', '', '', '', '', '', '', ''];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SquareComponent],
+  imports: [BrowserModule, AppRoutingModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
